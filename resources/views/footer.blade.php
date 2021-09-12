3ca8245823ca2f6e2feb4b7b928d75b133805ad4
jizya0208 <moridyan606@gmail.com>
2021-09-13 08:55:08 +0900

    ルーティング、基本ビュー画面の設定

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="container text-center">
+    <span class="text-light">©︎家計簿戦略</span>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// TOP画面を表示。「直近1ヵ月間の支出、収入の合計」「支出種別の円グラフ」「新規入力へのリンク」で構成。
+Route::get('/', 'RecordController@showList')->name
+('records');
+
+// 新規レコード入力画面 (支出/収入でタブ切替)
+Route::get('/record/create', 'RecordController@showCreate')->name
+('create');
+
+// 家計簿レコードの登録 
+Route::post('records/store', 'RecordController@exeStore')->name
+('store');
+
+// 家計簿レコードの編集
+Route::get('/record/edit/{id}', 'RecordController@showEdit')->name
+('edit');
+
+// 家計簿レコードの削除
+Route::delete('records/delete/{id}', 'RecordController@exeDelete')->name
+('delete');
